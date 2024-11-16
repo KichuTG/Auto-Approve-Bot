9f94bf089fd2387cbe28cede5d06310e4673c78a
KichuTG <kichumon333@outlook.com>
2024-11-16 19:58:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "your api id"))
     API_HASH = getenv("API_HASH", "your api hash")
-    BOT_TOKEN = getenv("BOT_TOKEN", "your bot token")
-    FSUB = getenv("FSUB", "your channel username")
-    CHID = int(getenv("CHID", "your channel id"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb url")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7553314720:AAFEEqWejYdZjJZTt2IXan-WNKt1Vz1wuFA")
+    FSUB = getenv("FSUB", "Dramaship")
+    CHID = int(getenv("CHID", "-1002039807510"))
+    SUDO = list(map(int, getenv("1350212613 1905251964").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://jiosaavn:jiosaavn@cluster0.ouhhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
