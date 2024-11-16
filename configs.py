404af96420db7213ee7a153b5edd3e034d6e3ef4
KichuTG <kichumon333@outlook.com>
2024-11-16 20:43:51 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "7553314720:AAFEEqWejYdZjJZTt2IXan-WNKt1Vz1wuFA")
     FSUB = getenv("FSUB", "Dramaship")
     CHID = int(getenv("CHID", "-1002039807510"))
-    SUDO = list(map(int, getenv("1350212613 1905251964").split()))
+    SUDO = list(map(int, getenv("1905251964").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://jiosaavn:jiosaavn@cluster0.ouhhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
