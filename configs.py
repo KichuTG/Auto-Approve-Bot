71b50113036b7925e644a1e8742b6282b2d0a1f7
KichuTG <kichumon333@outlook.com>
2024-11-16 20:06:57 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,8 +1,8 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "your api id"))
-    API_HASH = getenv("API_HASH", "your api hash")
+    API_ID = int(getenv("API_ID", "28714959"))
+    API_HASH = getenv("API_HASH", "c0b9797634090ee3f4c1c56db6c051a7")
     BOT_TOKEN = getenv("BOT_TOKEN", "7553314720:AAFEEqWejYdZjJZTt2IXan-WNKt1Vz1wuFA")
     FSUB = getenv("FSUB", "Dramaship")
     CHID = int(getenv("CHID", "-1002039807510"))
